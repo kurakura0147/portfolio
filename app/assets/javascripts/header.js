507cbe7731fbfd9401aef154cc13a0272d01013d
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-04 17:41:00 +0900

    View,jsの修正

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', function() {
+
+    //メニューのドロップダウン機能
     $(function(){
         $('.down-menu').hover(function(){
             $("ul:not(:animated)", this).slideDown();
@@ -6,4 +8,13 @@ $(document).on('turbolinks:load', function() {
             $("ul.child",this).slideUp();
         });
     });
+    //検索機能のドロップダウン
+    $(function(){
+        $('.down-menu').hover(function(){
+            $("ul:not(:animated)", this).slideDown();
+        }, function(){
+            $("ul.child-search",this).slideUp();
+        });
+    });
+
 });
