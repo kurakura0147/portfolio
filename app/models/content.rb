7daacc1118d6034f54ca006dd590ca9ab7cdb2f2
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-08 16:32:44 +0900

    アソシエーションの追加（タグ）

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,6 @@ class Content < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :likes, dependent: :destroy
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
 end
